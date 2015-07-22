490d0ea6a26dbef174905983b384bc49e964d8cf
ArrayDsj <arraydsj@163.com>
2015-07-23 03:06:33 +0800

    添加main方法
    添加测试代码

--- ATM/UserInfo.java
+++ ATM/UserInfo.java
@@ -1,13 +1,8 @@
 public class UserInfo {
-
     private String username;// 用户名
-
     private String cardNum;// 卡号
-
     private String password;// 密码
-
-    private float account;// 账户额
-
+    private float account;// 账户余额
     public UserInfo() {
 
     }
@@ -25,33 +20,25 @@ public class UserInfo {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getCardNum() {
         return cardNum;
     }
-
     public void setCardNum(String cardNum) {
         this.cardNum = cardNum;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public float getAccount() {
         return account;
     }
-
     public void setAccount(float account) {
         this.account = account;
     }
-
 }
