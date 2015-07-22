78eb2fc246967fcf67978e558d01e44793e59e43
ArrayDsj <arraydsj@163.com>
2015-07-23 02:42:59 +0800

    添加一个保存用户信息的类

---
+++ ATM/UserInfo.java
@@ -0,0 +1,57 @@
+public class UserInfo {
+
+    private String username;// 用户名
+
+    private String cardNum;// 卡号
+
+    private String password;// 密码
+
+    private float account;// 账户额
+
+    public UserInfo() {
+
+    }
+
+    public UserInfo(String username,
+            String cardNum,
+            String password,
+            float account) {
+        this.username = username;
+        this.cardNum = cardNum;
+        this.password = password;
+        this.account = account;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(String cardNum) {
+        this.cardNum = cardNum;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getAccount() {
+        return account;
+    }
+
+    public void setAccount(float account) {
+        this.account = account;
+    }
+
+}
