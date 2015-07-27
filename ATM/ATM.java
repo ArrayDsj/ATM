00244d93017ed30566056562aed4e310e6ace662
ArrayDsj <arraydsj@163.com>
2015-07-28 01:45:34 +0800

    使用Properties存储用户信息

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,65 +1,83 @@
-import java.util.Scanner;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Properties;
 import java.util.Scanner;
-/**
- * 使用正则表达式代替异常处理检查用户输入是否合法
- * 使用HashMap存储用户数据
- * 用卡号做键值,用户信息对象作为值
- * 添加打印凭条功能
- */
 
-class ATM {
-    // 此刻ATM内的现金
-    private int     cash;
-    // 用户信息
+public class ATM {
+    // 提款机现有现金
+    private int         cash;
+    // 提款机最大存储现金
+    private final int   MAX_CASH    = 100000;
+    // 当前操作用户
     private UserInfo    theUser;
-    // 使用HashMap存储用户数据
-    private HashMap<String, UserInfo>   UserInfoMap = new HashMap<String, UserInfo>();
-    // ATM能保存的最大现金数
-    public final int    MAX_CASH    = 100000;
+    //保存用户信息
+    Properties userInfoFile;
 
     public ATM() {
-        allUsers = new UserInfo[5];
-        for(int i = 0 ; i < 5; i++){
-            UserInfoMap.put("12345" + i ,
-                           new UserInfo("zhang"+i,
-                                        "12345"+i,
-                                        "12345"+i,
-                                        10000 + i * 1000));
+        this.cash = 50000;
+        // 加载用户信息文件
+        userInfoFile = new Properties();
+        try {
+            userInfoFile.load(new FileInputStream("UserInfo.properties"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        cash = 50000;
+
     }
 
-    // 运行--完成流程控制
+    // 写文件方法,当调用存取钱和更改密码时调用此方法
+    private void UpdateUserInfo(UserInfo theUser) {
+        // setProperty(String key, String value)
+        String temp = theUser.getUsername() + "@" + theUser.getCardNum() + "@"
+                + theUser.getPassword() + "@" + theUser.getAccount();
+        userInfoFile.setProperty(theUser.getCardNum(), temp);
+        // 写入文件
+        try {
+            userInfoFile.store(new FileOutputStream("UserInfo.properties"),
+                null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 控制流程
     public void run() {
         this.welcome();
         boolean flag = this.login();
 
         if (flag) {
+            new Load().loading();
             while (true) {
                 int choice = this.choiceMenu();
                 switch (choice) {
                     case 1:
-                        query();
+                        this.query();
                         break;
                     case 2:
-                        getMoney();
+                        this.getMoney();
                         break;
                     case 3:
-                        saveMoney();
+                        this.saveMoney();
                         break;
                     case 4:
-                        changPwd();
+                        this.changPwd();
                         break;
                     case 5:
-                        exit(0);
+                        this.exit(0);
                         break;
                     default:
                         System.out.println("只能输入数字1-5，请重新输入！");
+                        break;
                 }
             }
         } else {
@@ -69,37 +87,45 @@ class ATM {
 
     // 欢迎
     private void welcome() {
-        System.out.println("************************");
-        System.out.println("**********欢**迎*********");
-        System.out.println("**********使**用**********");
-        System.out.println("*********爱存不存*********");
-        System.out.println("**********银**行**********");
-        System.out.println("************************");
-        System.out.println("************************");
+        System.out.println("***************************");
+        System.out.println("**********欢***迎***********");
+        System.out.println("**********使***用***********");
+        System.out.println("**********爱存不存***********");
+        System.out.println("**********银***行***********");
+        System.out.println("***************************");
     }
 
     // 登录
     private boolean login() {
+        theUser = new UserInfo();
         Scanner scan = new Scanner(System.in);
+
         for (int i = 0; i < 3; i++) {
             System.out.println("请输入卡号：");
             String inputCardNum = scan.next();
             System.out.println("请输入密码：");
             String inputPwd = scan.next();
-
-
-            // 这里是用集合来装所有的元素
-            // 也可以用Set来装所有的键,然后遍历键,从而找到对应的那个元素的密码,然后对比
-            Collection<UserInfo> cl = UserInfoMap.values();
-            for (UserInfo user : cl) {
-                if (user.getCardNum().equals(inputCardNum)
-                        && user.getPassword().equals(inputPwd)) {
-                    System.out.println("登录成功！");
-                    theUser = user;
+            //如果userInfoFile中含有指定的键(对比卡号)
+            if(userInfoFile.containsKey(inputCardNum)){
+                //卡号正确,判断密码
+                //使用getProperty(String key) 用指定键找指定的值
+                String info = userInfoFile.getProperty(inputCardNum);
+                // 分解用户信息,存入theUser中
+                String[] temp = info.split("@");
+                theUser.setUsername(temp[0]);
+                theUser.setCardNum(temp[1]);
+                theUser.setPassword(temp[2]);
+                theUser.setAccount(Float.parseFloat(temp[3]));
+                //使用contains(),判断密码是否包含在用户信息中
+                if (theUser.getCardNum().equals(inputPwd)) {
+                    //成功匹配
                     return true;
+                }else{
+                    System.out.println("密码错误,您还有" + (2 - i) + "次机会！");
                 }
-            } // for循环体
-            System.out.println("卡号或密码错误,您还有" + (2 - i) + "次机会！");
+            }else{
+                System.out.println("账号错误,您还有" + (2 - i) + "次机会！");
+            }
         }
         System.out.println("三次卡号密码输入错误，卡被没收！");
         return false;
@@ -107,43 +133,43 @@ class ATM {
 
     // 选择菜单
     private int choiceMenu() {
-        while(true){
-                Scanner scan = new Scanner(System.in);
-                System.out.println("请选择您要操作的业务：");
-                System.out.println("1、查询余额；2、取款业务；3、存款业务；4、修改密码；5、退出");
-                String choice = scan.next();
-                boolean b = choice.matches("[1-5]{1,1}");
-                if(b){
-                    int a = Integer.parseInt(choice);
-                    return a;
-                }else {
-                    System.out.println("错误,请输入1-5之间的正整数!");
-                    System.out.println("是否继续?(y/n)");
-                    String s = new Scanner(System.in).next();
-                    if (s.matches("[y]|[Y]")) {
-                        continue;
-                    }
-                    if (s.matches("(n|N)")) {
-                        exit(0);
-                    } else {
-                        System.out.println("输入错误,谢谢使用");
-                        exit(0);
-                    }
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("请选择您要操作的业务：");
+            System.out.println("1、查询余额；2、取款业务；3、存款业务；4、修改密码；5、退出");
+            String choice = scan.next();
+            boolean b = choice.matches("[1-5]{1,1}");
+            if (b) {
+                int a = Integer.parseInt(choice);
+                return a;
+            } else {
+                System.out.println("错误,请输入1-5之间的正整数!");
+                System.out.println("是否继续?(y/n)");
+                String s = new Scanner(System.in).next();
+                if (s.matches("[y]|[Y]")) {
+                    continue;
+                }
+                if (s.matches("(n|N)")) {
+                    exit(0);
+                } else {
+                    System.out.println("输入错误,谢谢使用");
+                    exit(0);
                 }
             }
-    }
 
+        }
+
+    }
 
     // 查询
     private void query() {
-        System.out.println("账户余额:"
-                + theUser.getAccount() + "元.");
+        System.out.println("您当前的余额是：" + this.theUser.getAccount() + "元。");
     }
 
     // 存钱
     private void saveMoney() {
         int money = 0;
-        while(true){
+        while (true) {
             Scanner scan = new Scanner(System.in);
             System.out.println("请输入你要存入的金额：");
             String input = scan.next();
@@ -163,8 +189,12 @@ class ATM {
             money += inputMoney;
             this.theUser.setAccount(this.theUser.getAccount() + inputMoney);
             System.out.println("存钱操作已经成功，是否继续操作?(y/n)");
+
+            // 调用UpdateUserInfo()更新用户信息
+            UpdateUserInfo(theUser);
+
             String conti = new Scanner(System.in).next();
-            if(conti.matches("(y|Y)")){
+            if (conti.matches("(y|Y)")) {
                 continue;
             } else if (conti.matches("(n|N)")) {
                 System.out.println("是否打印凭条?(y/n)");
@@ -178,10 +208,11 @@ class ATM {
                     System.out.println("输入错误,谢谢使用");
                     exit(0);
                 }
-            }else{
+            } else {
                 System.out.println("输入错误,谢谢使用");
                 exit(0);
             }
+
         }
     }
 
@@ -212,6 +243,10 @@ class ATM {
             this.theUser.setAccount(this.theUser.getAccount() - outputMoney);
             this.cash -= outputMoney;
             money += outputMoney;
+
+            // 调用UpdateUserInfo()更新用户信息
+            UpdateUserInfo(theUser);
+
             System.out.println("取钱操作成功,是否继续操作(y/n)");
             String conti = new Scanner(System.in).next();
             if (conti.matches("(y|Y)")) {
@@ -228,6 +263,7 @@ class ATM {
                     System.out.println("输入错误,谢谢使用");
                     exit(0);
                 }
+
             }
         }
     }
@@ -252,6 +288,9 @@ class ATM {
         }
         this.theUser.setPassword(newPwd);
         System.out.println("新密码设置成功！");
+
+        // 调用UpdateUserInfo()更新用户信息
+        UpdateUserInfo(theUser);
     }
 
     // 打印凭条
@@ -267,10 +306,10 @@ class ATM {
     private void exit(int status) {
         if (status == 1) {
             System.out.println("本机停止为您服务，请去柜台操作处理！");
-            System.exit(0);
         } else if (status == 0) {
             System.out.println("谢谢您的使用，期待下次为您服务！");
-            System.exit(0);
         }
+        System.exit(0);
     }
 }
+
